03c310fbecd15050464522532e717f59bed0de19
gioqvarcxav4 <giokvarcxava@gmail.com>
2024-11-17 22:49:42 +0400

    second commit

---
+++ level13/class/GOA.py
@@ -0,0 +1,9 @@
+count = 2
+
+while 30 >= count > 0:
+    print(count)
+    count += 2
+    
+
+
+    
---
+++ level13/class/selitseq.py
@@ -0,0 +1,10 @@
+# Selection აძლევს მოწყობილობას ინსტრუქციას თუ რა შემთხვევაში რა გზა აირჩიოს
+# Sequence აირს თანმიმდევრობა
+# Iteration არის გამეორება
+
+
+Password = input('Enter your password: ')
+if Password== 'GiOrGi_6732':
+    print('Acces granted')
+else:
+    print('Access denied')
