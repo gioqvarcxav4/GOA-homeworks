eeced6e921ac5bd267c3a584b5b05d17c48e0843
gioqvarcxav4 <giokvarcxava@gmail.com>
2025-02-13 23:42:44 +0400

    twenty-second commit

---
+++ level 15/class/nolmain.py
@@ -0,0 +1,5 @@
+for i in range(0, 11):
+    i *= i + 1
+
+print(i // 2)
+
---
+++ level 17/class/user.py
@@ -0,0 +1,14 @@
+list = ['Me', 'animal', 'fruit', 'man', 'child', 'python', 'style', 'house', 'floor', 'eggs']
+new_list = list[:3]
+print(new_list)
+alt_list = list[-3:]
+print(alt_list)
+alt_list[0] = 'C#'
+alt_list[1] = 'JS'
+alt_list[2] = 'C++'
+alt_list.append('mango')
+alt_list.append('apple')
+alt_list.append('doctor')
+alt_list.append('book')
+alt_list.append('architect')
+print(alt_list)
