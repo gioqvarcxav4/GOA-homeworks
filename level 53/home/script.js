d67f2f44acd043e0dc722c28a254ba0cba60e30f
gioqvarcxav4 <giokvarcxava@gmail.com>
2025-04-22 15:50:52 +0400

    thirty-fourth commit

---
+++ level 53/home/script.js
@@ -0,0 +1,37 @@
+alert(10 + 10);
+alert(11 * 7);
+alert(33 / 3);
+alert(546 - 56);
+alert(12 * 99);
+alert(44 + 56);
+alert (345345 ** 9);
+alert(12 - 2);
+alert(1236372111909012368112 / 3);
+alert(1 + 1);
+
+console.log(10 + 10);
+console.log(11 * 7);
+console.log(33 / 3);
+console.log(546 - 56);
+console.log(12 * 99);
+console.log(44 + 56);
+console.log (345345 ** 9);
+console.log(12 - 2);
+console.log(1236372111909012368112 / 3);
+console.log(1 + 1);
+
+alert("The world will be destroyed in 10 seconds")
+alert("The world will be destroyed in 9 seconds")
+alert("The world will be destroyed in 8 seconds")
+alert("The world will be destroyed in 7 seconds")
+alert("The world will be destroyed in 6 seconds")
+alert("The world will be destroyed in 5 seconds")
+alert("The world will be destroyed in 4 seconds")
+alert("The world will be destroyed in 3 seconds")
+alert("The world will be destroyed in 2 seconds")
+alert("The world will be destroyed in 1 seconds")
+alert("The world will be destroyed in 0 seconds")
+
+// alert() ფუნქციას საიტზე გამოაქვს გაფრთხილება შესაბამისი ტექსტით
+// console.log() ფუნქციას გამოაქვს შესაბამისი ტექსტი console-ში
+//JavaScript არის ერთ-ერთი ყველაზე ცნობილი და გამოყენებული პროგრამირების ენა, რომელიც გამოიყენება როგორც საიტებისთვის ფუნქციის მისანიჭებლად, ასევე Back-End Development-ში
